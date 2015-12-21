3df2ac12edb2d9d1fc75bcab3570bfc73c42d753
Lewis Ngugi <ngugilewis@gmail.com>
2015-12-21 16:17:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
-// script.js
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-    // create the module and name it scotchApp
-        // also include ngRoute for all our routing needs
-    var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngAnimate']);
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
 
-    // configure our routes
-    scotchApp.config(function($routeProvider) {
-        $routeProvider
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
 
-            // route for the home page
-            .when('/', {
-                templateUrl : 'pages/home.html',
-                controller  : 'mainController'
-            })
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			});
+	});
 
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            })
-            .otherwise( {
-            	redirectTo: '/'
-            })
-            ;
-    });
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
 
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.pageClass = 'page-home';
-        $scope.message = 'Everyone come and see how good I look!';
-    });
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
 
-    scotchApp.controller('aboutController', function($scope) {
-        $scope.pageClass = 'page-about';
-        $scope.message = 'Look! I am an about page.';
-    });
-
-    scotchApp.controller('contactController', function($scope) {
-        $scope.pageClass = 'page-contact';
-        $scope.message = 'Contact us! JK. This is just a demo.';
-    });
+	scotchApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
