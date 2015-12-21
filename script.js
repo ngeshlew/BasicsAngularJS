538157cb25c63feaf0a394e7c9aad9ead3b181bc
ngeshlew <ngugilewis@gmail.com>
2015-12-21 14:50:08 +0300

    Added pageClass variable and ngAnimate Module for transition

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
     // create the module and name it scotchApp
         // also include ngRoute for all our routing needs
-    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+    var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngAnimate']);
 
     // configure our routes
     scotchApp.config(function($routeProvider) {
@@ -34,13 +34,16 @@
     // create the controller and inject Angular's $scope
     scotchApp.controller('mainController', function($scope) {
         // create a message to display in our view
+        $scope.pageClass = 'page-home';
         $scope.message = 'Everyone come and see how good I look!';
     });
 
     scotchApp.controller('aboutController', function($scope) {
+        $scope.pageClass = 'page-about';
         $scope.message = 'Look! I am an about page.';
     });
 
     scotchApp.controller('contactController', function($scope) {
+        $scope.pageClass = 'page-contact';
         $scope.message = 'Contact us! JK. This is just a demo.';
     });
