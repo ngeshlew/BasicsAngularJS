9aa40734c23aa91b12c058aac531a9ee9cb2846e
ngeshlew <ngugilewis@gmail.com>
2015-12-23 09:14:51 +0300

    Update files

--- javascript/routes.js
+++ javascript/routes.js
@@ -1,26 +1,26 @@
 	// configure our routes
-angular.module('scotchApp')
-	.config(function($routeProvider) {
-			$routeProvider
+	angular.module('scotchApp')
+	  .config(function($routeProvider) {
+	    $routeProvider
 
-				// route for the home page
-				.when('/', {
-					templateUrl : 'template/pages/home/index.html',
-					controller  : 'mainController'
-				})
+	    // route for the home page
+	      .when('/', {
+	      templateUrl: 'template/pages/home/index.html',
+	      controller: 'mainController'
+	    })
 
-				// route for the about page
-				.when('/about', {
-					templateUrl : 'template/pages/about/index.html',
-					controller  : 'aboutController'
-				})
+	    // route for the about page
+	    .when('/about', {
+	      templateUrl: 'template/pages/about/index.html',
+	      controller: 'aboutController'
+	    })
 
-				// route for the contact page
-				.when('/contact', {
-					templateUrl : 'template/pages/contact/index.html',
-					controller  : 'contactController'
-				})
-				.otherwise({
-					redirectTo: '/'
-				});
-		});
+	    // route for the contact page
+	    .when('/contact', {
+	        templateUrl: 'template/pages/contact/index.html',
+	        controller: 'contactController'
+	      })
+	      .otherwise({
+	        redirectTo: '/'
+	      });
+	  });
